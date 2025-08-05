1fce1b47a23ee9cfaf2d1194a3236013c0cf121f
JeffASDFGHJKL <imjuug@icloud.com>
2025-08-05 10:12:45 +0000

    Appaug5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
-import GeminusGame from "./components/GeminusGame";
+// Temporary test to see if React is loading at all.
 
 function App() {
-  return <GeminusGame />;
+  return (
+    <div style={{
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'blue',
+      color: 'white',
+      fontSize: '48px',
+      fontFamily: 'monospace'
+    }}>
+      REACT IS WORKING
+    </div>
+  );
 }
 
 export default App;
